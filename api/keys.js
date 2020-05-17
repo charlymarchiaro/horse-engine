48ffbd161b52ab2b522a0d365b34a12b54c96d9e
Charly Marchiaro <carlosmarchiaro@gmail.com>
2020-05-17 18:08:21 -0300

    updated api env variables names

--- api/keys.js
+++ api/keys.js
@@ -1,9 +1,9 @@
 module.exports = {
-  pgHost: process.env.PGHOST,
-  pgPort: process.env.PGPORT,
-  pgUser: process.env.PGUSER,
-  pgPassword: process.env.PGPASSWORD,
-  pgDatabase: process.env.PGDATABASE,
+  pgHost: process.env.PG_HOST,
+  pgPort: process.env.PG_PORT,
+  pgUser: process.env.PG_USER,
+  pgPassword: process.env.PG_PASSWORD,
+  pgDatabase: process.env.PG_DATABASE,
   scrapydHost: process.env.SCRAPYD_HOST,
   scrapydPort: process.env.SCRAPYD_PORT,
 };
