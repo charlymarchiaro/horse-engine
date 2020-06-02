dc896f1b3a55d6b473639d91c6101c20f4c948d3
Charly Marchiaro <carlosmarchiaro@gmail.com>
2020-06-02 18:30:42 -0300

    removed old api

--- _api/keys.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  pgHost: process.env.PG_HOST,
-  pgPort: parseInt(process.env.PG_PORT),
-  pgUser: process.env.PG_USER,
-  pgPassword: process.env.PG_PASSWORD,
-  pgDatabase: process.env.PG_DATABASE,
-  scrapydHost: process.env.SCRAPYD_HOST,
-  scrapydPort: process.env.SCRAPYD_PORT,
-};
