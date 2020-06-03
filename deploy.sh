cff316a19ca31d4b3ca75b5a17d4d5a62c918006
Charly Marchiaro <carlosmarchiaro@gmail.com>
2020-06-03 11:13:05 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,19 +2,19 @@ SHA=$(git rev-parse HEAD)
 
 docker build -t charlymarchiaro/horse-engine-client:latest -t charlymarchiaro/horse-engine-client:$SHA -f ./client/Dockerfile ./client
 docker build -t charlymarchiaro/horse-engine-api:latest -t charlymarchiaro/horse-engine-api:$SHA -f ./api/Dockerfile ./api
-# docker build -t charlymarchiaro/horse-engine-postgres:latest -t charlymarchiaro/horse-engine-postgres:$SHA -f ./postgres/Dockerfile ./postgres
+docker build -t charlymarchiaro/horse-engine-postgres:latest -t charlymarchiaro/horse-engine-postgres:$SHA -f ./postgres/Dockerfile ./postgres
 # docker build -t charlymarchiaro/pgadmin:latest -t charlymarchiaro/pgadmin:$SHA -f ./pgadmin/Dockerfile ./pgadmin
 docker build -t charlymarchiaro/horse-engine-scrapyd:latest -t charlymarchiaro/horse-engine-scrapyd:$SHA -f ./scrapyd/Dockerfile ./scrapyd
 
 docker push charlymarchiaro/horse-engine-client:latest
 docker push charlymarchiaro/horse-engine-api:latest
-# docker push charlymarchiaro/horse-engine-postgres:latest
+docker push charlymarchiaro/horse-engine-postgres:latest
 # docker push charlymarchiaro/pgadmin:latest
 docker push charlymarchiaro/horse-engine-scrapyd:latest
 
 docker push charlymarchiaro/horse-engine-client:$SHA
 docker push charlymarchiaro/horse-engine-api:$SHA
-# docker push charlymarchiaro/horse-engine-postgres:$SHA
+docker push charlymarchiaro/horse-engine-postgres:$SHA
 # docker push charlymarchiaro/pgadmin:$SHA
 docker push charlymarchiaro/horse-engine-scrapyd:$SHA
 
@@ -53,13 +53,13 @@ kubectl \
   --token=$KUBERNETES_TOKEN \
   set image deployments/api-deployment api=charlymarchiaro/horse-engine-api:$SHA
 
-# # # postgres
-# # kubectl \
-# #   --kubeconfig=/dev/null \
-# #   --server=$KUBERNETES_SERVER \
-# #   --certificate-authority=cert.crt \
-# #   --token=$KUBERNETES_TOKEN \
-# #   set image deployments/postgres-deployment postgres=charlymarchiaro/horse-engine-postgres:$SHA
+# postgres
+kubectl \
+  --kubeconfig=/dev/null \
+  --server=$KUBERNETES_SERVER \
+  --certificate-authority=cert.crt \
+  --token=$KUBERNETES_TOKEN \
+  set image deployments/postgres-deployment postgres=charlymarchiaro/horse-engine-postgres:$SHA
 
 # # # pgadmin
 # # kubectl \
