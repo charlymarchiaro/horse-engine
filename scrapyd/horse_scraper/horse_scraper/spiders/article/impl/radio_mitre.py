a27f8335484e4e029a5a54876627d88247a65961
Charly Marchiaro <carlosmarchiaro@gmail.com>
2020-06-08 22:58:42 -0300

    minor fix

--- scrapyd/horse_scraper/horse_scraper/spiders/article/impl/radio_mitre.py
+++ scrapyd/horse_scraper/horse_scraper/spiders/article/impl/radio_mitre.py
@@ -1,4 +1,4 @@
-from typing import Tuple, List, Dict, Union, Callable, cast
+from typing import Tuple, List, Dict, Any, Union, Callable, cast
 
 import json
 import html
@@ -62,7 +62,7 @@ class Params(BaseArticleSpiderParams):
     def get_sitemap_follow(self) -> List[str]:
         return [".*"]
 
-    def should_parse_sitemap_entry(self, entry: Dict[str, str]) -> bool:
+    def should_parse_sitemap_entry(self, entry: Any) -> bool:
         return True
 
     def should_follow_sitemap_url(self, url: str) -> bool:
