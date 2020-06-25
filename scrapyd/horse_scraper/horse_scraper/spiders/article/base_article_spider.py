ab108ee4e49265ec04d2928617ee6fe28d9a17af
Charly Marchiaro <carlosmarchiaro@gmail.com>
2020-06-25 20:35:47 -0300

    fix: variable initialized at start

--- scrapyd/horse_scraper/horse_scraper/spiders/article/base_article_spider.py
+++ scrapyd/horse_scraper/horse_scraper/spiders/article/base_article_spider.py
@@ -139,7 +139,7 @@ class BaseArticleSpider:
         parser_functions: List[Callable[[HtmlResponse], ArticleData]],
     ) -> Tuple[Union[datetime, None], Union[ArticleData, None], Union[str, None]]:
 
-        article_date: Union[datetime, None]
+        article_date: Union[datetime, None] = None
 
         for f in parser_functions:
 
