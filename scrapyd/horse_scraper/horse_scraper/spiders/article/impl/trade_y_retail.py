1a59895b61e4c6daed4f97d481563a76b8d445d2
Charly Marchiaro <carlosmarchiaro@gmail.com>
2020-06-26 16:07:05 -0300

    spiders impl

---
+++ scrapyd/horse_scraper/horse_scraper/spiders/article/impl/trade_y_retail.py
@@ -0,0 +1,125 @@
+from typing import Tuple, List, Dict, Any, Union, Callable, cast
+
+import json
+import html
+import dateparser  # type: ignore
+from datetime import datetime, date, timedelta
+from string import whitespace
+import re
+
+from scrapy.http import Request, HtmlResponse  # type: ignore
+from scrapy.linkextractors import LinkExtractor  # type: ignore
+from scrapy.spiders import Rule  # type: ignore
+
+from horse_scraper.items import Article
+from horse_scraper.spiders.article.model import ArticleData, SpiderType
+from horse_scraper.spiders.article.base_article_spider_params import (
+    BaseArticleSpiderParams,
+    UrlFilter,
+)
+from horse_scraper.services.utils.parse_utils import (
+    extract_all_text,
+    AttributeType,
+    get_publishing_date,
+)
+from ..base_article_crawl_spider import BaseArticleCrawlSpider
+from ..base_article_sitemap_spider import BaseArticleSitemapSpider
+
+
+class Params(BaseArticleSpiderParams):
+    def _after_initialize(self) -> None:
+        pass
+
+    # Common params
+    def _get_spider_base_name(self) -> str:
+        return "trade_y_retail"
+
+    def get_allowed_domains(self) -> List[str]:
+        return ["tradeyretail.com"]
+
+    # Crawl params
+
+    def get_crawl_start_urls(self) -> List[str]:
+        return [
+            "https://tradeyretail.com/",
+            "https://tradeyretail.com/industria/",
+            "https://tradeyretail.com/protagonistas/",
+            "https://tradeyretail.com/investigaciones/",
+            "https://tradeyretail.com/lanzamientos/",
+            "https://tradeyretail.com/soluciones/",
+        ]
+
+    def get_url_filter(self) -> UrlFilter:
+        return UrlFilter(allow_re=[".*\/\d+_.*"], deny_re=[])
+
+    # Sitemap params
+
+    def get_sitemap_urls(self) -> List[str]:
+        return []
+
+    def get_sitemap_follow(self) -> List[str]:
+        return [".*"]
+
+    def should_parse_sitemap_entry(self, entry: Any) -> bool:
+        return True
+
+    def should_follow_sitemap_url(self, url: str) -> bool:
+        return True
+
+    def should_follow_article_url(self, url: str) -> bool:
+
+        try:
+            # Articles do not have publish dates. Filtering by id (integer) present in URL
+            match = re.search(".*\/(\d+)_.*", url)
+            if match:
+                article_id = int(match.group(1))
+                if article_id > 800:
+                    return True
+            return False
+
+        except:
+            return False
+
+    # Parser functions
+
+    def get_parser_functions(self) -> List[Callable[[HtmlResponse], ArticleData]]:
+        return [
+            self.parser_1,
+        ]
+
+    def parser_1(self, response):
+
+        article_data = self.get_default_parser_results(response)
+
+        title = article_data.title
+
+        # Articles do not have publish dates. Using current date 
+        # (plus filtering by id in URL)
+        last_updated = datetime.now()
+
+        # text ----------
+        text = extract_all_text(
+            response,
+            root_xpath="//*",
+            exclude_list=[
+                (AttributeType.NAME, "script"),
+                (AttributeType.NAME, "style"),
+                (AttributeType.CLASS, "nav"),
+                (AttributeType.CLASS, "card"),
+            ],
+        )
+
+        return ArticleData(title, text, last_updated)
+
+
+# Spider implementations
+
+
+class CrawlSpider(BaseArticleCrawlSpider):
+    params = Params()
+    name = params.get_spider_name(SpiderType.CRAWL)
+
+
+class SitemapSpider(BaseArticleSitemapSpider):
+    params = Params()
+    name = params.get_spider_name(SpiderType.SITEMAP)
