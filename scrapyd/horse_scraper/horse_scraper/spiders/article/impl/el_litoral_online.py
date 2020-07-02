6f96bd8518a67559f04d34ecdd674b5109fe7ca8
Charly Marchiaro <carlosmarchiaro@gmail.com>
2020-07-02 12:42:27 -0300

    Update el_litoral_online.py

--- scrapyd/horse_scraper/horse_scraper/spiders/article/impl/el_litoral_online.py
+++ scrapyd/horse_scraper/horse_scraper/spiders/article/impl/el_litoral_online.py
@@ -42,6 +42,21 @@ class Params(BaseArticleSpiderParams):
     def get_crawl_start_urls(self) -> List[str]:
         return [
             "https://www.ellitoral.com/",
+            "https://www.ellitoral.com/index.php/titulos",
+            "https://www.ellitoral.com/index.php/um/area_metropolitana",
+            "https://www.ellitoral.com/index.php/um/sucesos",
+            "https://www.ellitoral.com/index.php/um/deportes",
+            "https://www.ellitoral.com/index.php/um/politica",
+            "https://www.ellitoral.com/index.php/um/opinion",
+            "https://www.ellitoral.com/index.php/temas/Filtrado",
+            "https://www.ellitoral.com/index.php/temas/Tecno",
+            "https://www.ellitoral.com/index.php/um/sabalera",
+            "https://www.ellitoral.com/index.php/um/tatengue",
+            "https://www.ellitoral.com/index.php/participacion/periodismo",
+            "https://www.ellitoral.com/index.php/participacion/",
+            "https://www.ellitoral.com/index.php/servicios",
+            "https://www.ellitoral.com/index.php/servicios/clima",
+            "https://www.ellitoral.com/index.php/temas/El-Litoral-Podcasts",
         ]
 
     def get_url_filter(self) -> UrlFilter:
@@ -71,7 +86,29 @@ class Params(BaseArticleSpiderParams):
     # Parser functions
 
     def get_parser_functions(self) -> List[Callable[[HtmlResponse], ArticleData]]:
-        return []
+        return [
+            self.parser_1,
+        ]
+
+    def parser_1(self, response):
+
+        article_data = self.get_default_parser_results(response)
+
+        title = article_data.title
+        last_updated = article_data.last_updated
+
+        # text ----------
+        text = extract_all_text(
+            response,
+            root_xpath='//div[contains(@class, "cuerpo_interior")]',
+            exclude_list=[
+                (AttributeType.NAME, "script"),
+                (AttributeType.NAME, "style"),
+                (AttributeType.CLASS, "relacionada"),
+            ],
+        )
+
+        return ArticleData(title, text, last_updated)
 
 
 # Spider implementations
