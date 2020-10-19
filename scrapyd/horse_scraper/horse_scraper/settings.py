f1a85d4164df2b51888fd2fecf2609d4303b1113
Charly Marchiaro <carlosmarchiaro@gmail.com>
2020-10-19 19:56:04 -0300

    Update settings.py

--- scrapyd/horse_scraper/horse_scraper/settings.py
+++ scrapyd/horse_scraper/horse_scraper/settings.py
@@ -117,7 +117,7 @@ HTTPCACHE_STORAGE = "scrapy_splash.SplashAwareFSCacheStorage"
 
 FEED_EXPORT_ENCODING = "utf-8"
 
-LOG_LEVEL = "INFO"
+LOG_LEVEL = os.environ.get("SCRAPYD_LOG_LEVEL")
 
 # App params
 CRAWL_PERIOD_DAYS_BACK = 15
