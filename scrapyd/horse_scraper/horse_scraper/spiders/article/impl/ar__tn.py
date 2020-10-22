aae28beaf65506b040585ad09406c2a5b9b99128
Charly Marchiaro <carlosmarchiaro@gmail.com>
2020-10-22 18:55:46 -0300

    Update ar__tn.py

--- scrapyd/horse_scraper/horse_scraper/spiders/article/impl/ar__tn.py
+++ scrapyd/horse_scraper/horse_scraper/spiders/article/impl/ar__tn.py
@@ -62,7 +62,7 @@ class Params(BaseArticleSpiderParams):
         ]
 
     def get_url_filter(self) -> UrlFilter:
-        return UrlFilter(allow_re=[f".*({self.date_allow_str}).+_story"], deny_re=[])
+        return UrlFilter(allow_re=[f".*({self.date_allow_str}).+"], deny_re=[])
 
     # Sitemap params
 
