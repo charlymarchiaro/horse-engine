35b9c94843f93a6037d7a9a4c89d3d9bce238192
Charly Marchiaro <carlosmarchiaro@gmail.com>
2020-10-27 10:29:02 -0300

    Update mx__el_universal.py

--- scrapyd/horse_scraper/horse_scraper/spiders/article/impl/mx__el_universal.py
+++ scrapyd/horse_scraper/horse_scraper/spiders/article/impl/mx__el_universal.py
@@ -62,7 +62,9 @@ class Params(BaseArticleSpiderParams):
         ]
 
     def get_url_filter(self) -> UrlFilter:
-        return UrlFilter(allow_re=[".*eluniversal.com.mx\/.+\/.{10,}"], deny_re=[])
+        return UrlFilter(
+            allow_re=[".*eluniversal.com.mx\/.+\/.{10,}"], deny_re=["page="]
+        )
 
     # Sitemap params
 
