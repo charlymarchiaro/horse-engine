233db1a0574911d0db9a3818e40b41129930028f
Charly Marchiaro <carlosmarchiaro@gmail.com>
2020-12-09 19:32:19 -0300

    Update ar__el_litoral_online.py

--- scrapyd/horse_scraper/horse_scraper/spiders/article/impl/ar__el_litoral_online.py
+++ scrapyd/horse_scraper/horse_scraper/spiders/article/impl/ar__el_litoral_online.py
@@ -37,6 +37,9 @@ class Params(BaseArticleSpiderParams):
     def get_allowed_domains(self) -> List[str]:
         return ["ellitoral.com"]
 
+    def pre_process_url(self, url) -> str:
+        return url.replace("/m.ellitoral.com/", "/www.ellitoral.com/")
+
     # Crawl params
 
     def get_crawl_start_urls(self) -> List[str]:
